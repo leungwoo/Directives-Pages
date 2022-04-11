132a75f0582003cb94d4db9744dc6612f3205c5d
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-11 11:01:50 -0400

    Pre and Next button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,4 +30,6 @@ export class AppComponent {
       url: 'https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dW5pdmVyc2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
     },
   ];
+
+  onClick() {}
 }
