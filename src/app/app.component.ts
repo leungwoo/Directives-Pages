8efe28b3bdbd7eb9bac32d5003176e9a4f948a9b
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-11 09:46:32 -0400

    Add ngClass to how active page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public currentPage = 0;
   images = [
     {
       title: 'At the beach',
